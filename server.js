89a084254c89e70e91c8609853052540d8e9fe74
Mirza Maksumić <mirza.makso@gmail.com>
2023-05-20 17:48:51 +0200

    Third commit

--- server.js
+++ server.js
@@ -58,6 +58,50 @@ app.get('/weather/forecast', async (req, res) => {
 });
 
 
+
+// history
+app.get('/weather/history', async (req, res) => {
+  const location = req.query.location;
+  const startDate = req.query.start_date;
+  const endDate = req.query.end_date;
+
+  if (!location) {
+    return res.status(400).json({ error: 'Location parameter is missing' });
+  }
+
+  if (!startDate || !endDate) {
+    return res.status(400).json({ error: 'Start date and/or end date parameters are missing' });
+  }
+
+  // get latitude and longitude for the location
+  try {
+    const locationResponse = await axios.get(`http://api.openweathermap.org/data/2.5/history/city?q=${location}&type=hour&start=${startDate}&end=${endDate}&appid=${api_key}`);
+    const { lat, lng } = locationResponse.data.results[0].geometry.location;
+
+    // convert start and end date to Unix timestamps
+    const startTimestamp = new Date(startDate).getTime() / 1000;
+    const endTimestamp = new Date(endDate).getTime() / 1000;
+
+    // make call to OpenWeatherMap API to get historical weather data
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lng}&dt=${startTimestamp}&appid=${api_key}`);
+    const historyData = response.data;
+    return res.json({
+      provider: 'OpenWeatherMap',
+      lastRefreshed: new Date(),
+      data: {
+        temperature: historyData.current.temp,
+        humidity: historyData.current.humidity,
+        description: historyData.current.weather[0].description,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ error: 'Error retrieving weather data' });
+  }
+});
+
+
+
+
 app.listen(3000, () => {
   console.log('Weather app listening on port 3000!');
 });
